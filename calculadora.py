2766393fad2dde8bddb3ab7816e790ffae3879fe
rebecagoulartt <rebeca.goulart@leanwork.com.br>
2026-04-17 15:43:55 -0300

    adicionando funções básicas da calculadora

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b == 0:
+        return "Erro: divisão por zero!"
+    return a / b
+
+print("=== Calculadora ===")
+print("Soma 2+3 =", soma(2, 3))
+print("Subtração 10-4 =", subtracao(10, 4))
+print("Multiplicação 3x5 =", multiplicacao(3, 5))
+print("Divisão 10/2 =", divisao(10, 2))

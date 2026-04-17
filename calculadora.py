0f9816ffd5467b7d1a90636c24eb25fb5e4a435c
rebecagoulartt <rebeca.goulart@leanwork.com.br>
2026-04-17 15:46:18 -0300

    adicionando função de potência

--- calculadora.py
+++ calculadora.py
@@ -17,3 +17,8 @@ print("Soma 2+3 =", soma(2, 3))
 print("Subtração 10-4 =", subtracao(10, 4))
 print("Multiplicação 3x5 =", multiplicacao(3, 5))
 print("Divisão 10/2 =", divisao(10, 2))
+
+def potencia(base, expoente):
+    return base ** expoente
+
+print("Potência 2^10 =", potencia(2, 10))
